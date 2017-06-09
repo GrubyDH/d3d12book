1cb8845785a5106868318ccc124e7f55e38bcb53
Pawel Kozlowski <pawel.kozlowski@outlook.com>
2017-06-10 01:39:45 +0200

    Add SSAO debug shader.

---
+++ MySamples/Ssao/Shaders/SsaoDebug.hlsl
@@ -0,0 +1,36 @@
+//***************************************************************************************
+// ShadowDebug.hlsl by Frank Luna (C) 2015 All Rights Reserved.
+//***************************************************************************************
+
+// Include common HLSL code.
+#include "Common.hlsl"
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+	float2 TexC    : TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH    : SV_POSITION;
+	float2 TexC    : TEXCOORD;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout = (VertexOut)0.0f;
+
+	// Already in homogeneous clip space.
+	// Use gWorld as additional transformation within homogenous clip space.
+	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorld);
+
+	vout.TexC = vin.TexC;
+
+	return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+	return float4(gSsaoMap.Sample(gsamLinearWrap, pin.TexC).rrr, 1.0f);
+}
